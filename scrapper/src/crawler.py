ac3ca0f084a85f1a4c81a73a931c5b6c0e12ebd2
Martin Cerdeira <mcerdeira@provincianet.com.ar>
2019-07-26 15:36:11 -0300

    Non recursive version

--- scrapper/src/crawler.py
+++ scrapper/src/crawler.py
@@ -18,16 +18,29 @@ LOG_FILE = None
 
 
 def start_crawl():
+    global urls
+    last_url = ""
     intial_url_load()
-    for url in urls:
-        robots = get_robots(url[1])
+    i = 0
+    while(True):
+        url = urls[i]
+        print(str(len(urls)))
+        if(not url[1].startswith("/") and url[0] != last_url):
+            last_url = url[0]
+            robots = get_robots(url[1])
         crawl(robots, url)
+        urls = list(set(urls))
+        i += 1
+        if i - 1 >= len(urls):
+            break
 
 def intial_url_load():
+    global urls
     sql = "SELECT url FROM result"
     rows = _cursor.execute(sql)
     for r in rows.fetchall():
-        if r[0] not in urls:
+        found = [u for u in urls if u[1] == r[0]]
+        if(len(found) == 0):
             already_fetched.append(r[0])
 
 def get_robots(url):
@@ -43,31 +56,33 @@ def get_robots(url):
 
 
 def crawl(robots, url):
+    global urls
     base_url = url[0]
     global count
     count += 1
     try:
         # Is same domain or relative URL
-        if base_url in url[1] or url.startswith("/"):
+        if base_url in url[1] or url[1].startswith("/"):
             valid = False
-            if(url.startswith("/")):
-                url = base_url + url[1]
+            if(url[1].startswith("/")):                
+                tmpurl = url[1]
+                url = (base_url, base_url + tmpurl)
                 valid = True
             else:
                 url1 = base_url
                 url2 = base_url.replace("https", "http")
-                if(url.startswith(url1) or url.startswith(url2)):
+                if(url[1].startswith(url1) or url[1].startswith(url2)):
                     valid = True
                 else:
                     valid = False
 
-            if(valid and not url[1] in found_urls and (robots.can_fetch("*", url) or robots is None)):
+            if(valid and not url[1] in found_urls and (robots.can_fetch("*", url[1]) or robots is None)):
                 html = urlopen(url[1])
                 print(url[1])
                 found_urls.append(url[1])
                 dump(robots, url, html)
             else:
-                print("Skiped: " + url)
+                print("Skiped: " + url[1])
 
     except Exception as e:
         found_urls.append(url[1])
@@ -81,24 +96,36 @@ def crawl(robots, url):
 
 
 def dump(robots, url, content):
-    base_url = url[0]
-    soup = BeautifulSoup(content, 'html.parser')
-    title = soup.title.text
+    global urls
+    try:
+        base_url = url[0]
+        soup = BeautifulSoup(content, 'html.parser')
+        if(soup.title):
+            title = soup.title.text
+        else:
+            title = ""
 
-    if not url[1] in already_fetched:
-        already_fetched.append(url[1])
-        sql = "INSERT INTO result VALUES (?, ?, ?)"
-        _cursor.execute(sql, (url[1], title, soup.prettify(), ))
+        if not url[1] in already_fetched:
+            already_fetched.append(url[1])
+            sql = "INSERT INTO result VALUES (?, ?, ?)"
+            _cursor.execute(sql, (url[1], title, soup.prettify(), ))
 
-        if(count % COMMIT_EVERY == 0):
-            conn.commit()
+            if(count % COMMIT_EVERY == 0):
+                conn.commit()
 
-    for link in soup.find_all('a'):
-        newlink = link.get('href')
-        if(newlink and newlink not in found_urls):
-            urls.append((base_url, newlink))
-            #crawl(robots, base_url, newlink.strip())
+        for link in soup.find_all('a'):
+            newlink = link.get('href')
+            if(newlink and newlink not in found_urls):
+                urls.append((base_url, newlink))            
+                #crawl(robots, base_url, newlink.strip())
+    except Exception as e:
+        LOG_FILE.write(url[1] + ": " + str(e) + " " + repr(e) +
+                       " " + traceback.format_exc() + "\n\n")
+        LOG_FILE.write("---------------------------------------------")
 
+        print("ERROR: " + str(e))
+        print(repr(e))
+        traceback.print_exc()
 
 if __name__ == '__main__':
     try:
