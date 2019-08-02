a2c36464e4f5ad603e931a72b1cb15c91fc55136
Martin Cerdeira <mcerdeira@provincianet.com.ar>
2019-08-02 10:32:38 -0300

    Spacing

--- search engine app/src/components/SearchBar.vue
+++ search engine app/src/components/SearchBar.vue
@@ -1,14 +1,14 @@
 <template>
-	<div class="container">
+    <div class="container">
         <input class="form-control input-sm" @keyup.enter="trigger_click" autofocus v-model="query" id="search_text" type="text">
-		<br>
+        <br>
         <button class="btn btn-light" type="submit" @click="search_clicked" ref="button_search">Search</button>
         <br>
         <br>
         <div v-for="v in result" :key="v.title" >
             <a :href="v.url" target="_blank">{{v.title}}</a>
         </div>
-	</div>       
+    </div>       
 </template>
 
 <script>
